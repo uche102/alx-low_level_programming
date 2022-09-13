0c8eaab88d92b381651fdad0433560f8beee8c05
uche102 <nwakoruche192@gmail.com>
2022-09-13 15:29:43 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -13,8 +13,14 @@ int print_sign(int n)
 }
         else if (n < 0)
         {
-	_putchar('_')
+	_putchar('_');
         return (-1);
 	}
         else
+	{
+	_putchar('0');
+	return (0);
+	}
+}
+
                               
