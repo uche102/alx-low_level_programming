f8a990b0318dd08aba204086fe02885e154bb61a
uche102 <nwakoruche192@gmail.com>
2022-09-20 19:41:56 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -15,11 +15,8 @@ void rev_string(char *s)
                     tmp = *(s + i);
 		    *(s + i) = *(s + len - i - 1);
 		    *(s + len - i - 1) = tmp;
-             }
-
+             }    
 }
-
-
 
 /**
  *
