3bd62886f31beaa0de501c114190960bdaa6f34f
uche102 <nwakoruche192@gmail.com>
2022-09-22 15:28:55 -0700

    0-strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -6,7 +6,7 @@
  *@dest: String that will be appended
  *@src: String to be concatenated upon
  *
- * Return: returns pointer to @dest 
+ * Return: returns poiner to @dest 
  */
 
 char *_strcat(char *dest, char *src)
@@ -14,7 +14,7 @@ char *_strcat(char *dest, char *src)
 
         int index = 0, dest_len = 0;
 
-	while (dest[index++])
+        while (dest[index++])
 		dest_len++;
 	
 	for (index = 0; src[index]; index++)
