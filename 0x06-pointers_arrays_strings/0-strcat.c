f8388cafc490583a8c4ec8de9466c704395e2312
uche102 <nwakoruche192@gmail.com>
2022-09-22 15:02:43 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -18,8 +18,8 @@ char *_strcat(char *dest, char *src)
 		dest_len++;
 
 	for (index = 0; src[index]; index++)
-		dest[dest_len++] = src[index];
-	
+		dest[dest_len++] = src[index];	
 	return (dest);
 }
+
 
