dd4129721efb0fe0e8217ed8927cc9a09c26b077
uche102 <nwakoruche192@gmail.com>
2022-09-22 18:50:09 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -6,8 +6,8 @@
 *@s2:second string
 *Return:
 *		returns zero if s1 == s2
-* 	returns negative number if s1 < s2
-* 	returns positive number if s1 > s2
+* returns negative number if s1 < s2
+* returns positive number if s1 > s2
 */
 
 int _strcmp(char *s1, char *s2)
@@ -36,6 +36,6 @@ int _strcmp(char *s1, char *s2)
 					else
 			i++;
 
-				 }
-				 return (diff}
+		}
+										  return (diff}
 }
