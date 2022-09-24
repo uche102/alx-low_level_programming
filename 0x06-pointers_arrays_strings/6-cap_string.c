cf0b514bffbd2cbf8cb632159ebe7360c1558c23
uche102 <nwakoruche192@gmail.com>
2022-09-23 18:46:24 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -20,9 +20,9 @@ char *cap_string(char *s)
 				*(s + i) -= 'a' - 'A';
 			else
 			{
-				        for (j = 0; j <= 12; j++)
+			for (j = 0; j <= 12; j++)
 						{
-					if (a[j] == *(s + i - 1))
+			if (a[j] == *(s + i - 1))
 								*(s + i) -= 'a' - 'A';
 						}
 					}
