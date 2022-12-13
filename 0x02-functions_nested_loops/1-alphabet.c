a579c04944ccd997a622a03def8b3213b91184b7
uche102 <nwakoruche192@gmail.com>
2022-12-14 00:17:40 +0100

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,13 +6,13 @@
  */
 void print_alphabet(void)
 {
-        char c;
-        
-        for (c = 'a'; c <= 'z'; c++)
-        {
-                _putchar(c);
-	}         
+char c;
+
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
 _putchar('\n');
 }
-        
+
 
