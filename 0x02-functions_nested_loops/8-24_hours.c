87041670a00751cf33b72827118b6e7282b2682c
uche102 <nwakoruche192@gmail.com>
2022-12-14 01:40:36 +0100

    Jack Bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,11 +9,11 @@ void jack_bauer(void)
 {
 	int hour, minute;
 
-	for (hour = 0; hour <= 23; hour++
+	for (hour = 0; hour <= 23; hour++)
 	{
 	
 	         for (minute = 0; minute <= 59; minute++)
-		  {
+		 {
 
 		          _putchar((hour / 10) + '0');
 			  _putchar((hour % 10) + '0');
@@ -21,7 +21,7 @@ void jack_bauer(void)
 			  _putchar((minute / 10) + '0');
 			  _putchar((minute % 10) + '0');
 			  _putchar('\n')
-		  }
+		 }
 		               
          }
 }
