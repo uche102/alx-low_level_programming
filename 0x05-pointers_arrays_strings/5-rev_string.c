d10664b73c73530181cd87c78e31c6c696566745
uche102 <nwakoruche192@gmail.com>
2022-12-20 01:32:03 +0100

    rev string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,14 +10,14 @@
 
 void rev_string(char *s)
 {
-             int i, tmp, len = _strlen(s);
+int i, tmp, len = _strlen(s);
 
-             for (i = 0; i < len / 2; i++)
-             {
-                    tmp = *(s + i);
+for (i = 0; i < len / 2; i++)
+{
+tmp = *(s + i);
 		    *(s + i) = *(s + len - i - 1);
 		    *(s + len - i - 1) = tmp;
-             }    
+}
 }
 
 /**
@@ -29,10 +29,10 @@ void rev_string(char *s)
 
 int _strlen(char *s)
 {
-        int len = 0;
+int len = 0;
 
 	while (*(s + len) != '\0')
-                len++;
-		
+len++;
+
 	return (len);
 }
