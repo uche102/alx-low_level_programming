c0dac5ce05b3ee07d2e26f094c9bfc02352263c2
uche102 <nwakoruche192@gmail.com>
2022-12-20 01:47:57 +0100

    strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -10,18 +10,17 @@
 
 char *_strcpy(char *dest, char *src)
 {
-        int inc = 0; 
-		
-        while (*(src + inc) != '\0')
-        {
-					
-                *(dest + inc) = *(src + inc);
-                inc++;
+int inc = 0;
+
+while (*(src + inc) != '\0')
+{
+
+*(dest + inc) = *(src + inc);
+inc++;
 	}
 	*(dest + inc) = '\0';
 
 
 	return (dest);
 }
-                                         
 
