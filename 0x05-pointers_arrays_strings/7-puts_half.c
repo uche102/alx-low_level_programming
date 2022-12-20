0f7e61bb2e1a2152b0c39f10b81d57a92503a8e5
uche102 <nwakoruche192@gmail.com>
2022-12-20 01:42:17 +0100

    puts half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,14 +9,14 @@
  */
 
 void puts_half(char *str)
-{     
+{
 	int i, len = _strlen(str);
-	
+
 	for (i = ((len - 1) / 2) + 1; i < len; i++)
-                                            
+
 	        putchar(*(str + i));
 
-        putchar(10);
+putchar(10);
 }
 
 /**
@@ -28,10 +28,10 @@ void puts_half(char *str)
 
 int _strlen(char *s)
 {
-        int len = 0;
+int len = 0;
 
-        while (*(s + len) != '\0')
-                len++;
+while (*(s + len) != '\0')
+len++;
 
-        return (len);
+return (len);
 }
