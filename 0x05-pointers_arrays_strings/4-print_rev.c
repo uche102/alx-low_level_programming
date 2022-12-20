9a6fc85e457c22b1ac0d0431cea43343a224d142
uche102 <nwakoruche192@gmail.com>
2022-12-20 01:28:30 +0100

    print rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,9 +10,9 @@
 
 void print_rev(char *s)
 {
-        int len = strlen(s);
+int len = strlen(s);
 
-        while (len--)
-                putchar(*(s + len));
-        putchar(10);
+while (len--)
+putchar(*(s + len));
+putchar(10);
 }
