62065eaf03662b96e447f861cecb077f006d1d9a
uche102 <nwakoruche192@gmail.com>
2022-12-20 01:25:19 +0100

    puts

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -11,12 +11,12 @@
 
 void _puts(char *str)
 {
-        int i = 0;
-          
-        while (*(str + i) != '\0')
-        {
-                putchar(*(str + i));
-                i++;
-        }
-        putchar(10);
+int i = 0;
+
+while (*(str + i) != '\0')
+{
+putchar(*(str + i));
+i++;
+}
+putchar(10);
 }
