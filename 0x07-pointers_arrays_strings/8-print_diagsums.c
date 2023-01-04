c2faa7af92ce37fb4a8098e8cc751602521c8bea
uche102 <nwakoruche192@gmail.com>
2023-01-04 02:05:17 +0100

    print diagsums

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,26 +1,19 @@
-#include "main.h"
-#include <stdio.h>
+#include "main.h"                                                               #include <stdio.h>                                                              
 
-/**
- * print_diagsums - prints the sums of two diagonals of a square matrix
- * @a: pointer to start of matrizx
- * @size:  width of the matrix column
- *
- * Return: void
- */
-void print_diagsums(int *a, int size)
-{
-	int i, j, p, l = 0, r = 0;
-
-	for (i = 0; i < size; i++)
-	{
-		p = (i * size) + i;
-		l += *(a + p);
-	}
-	for (j = 0; j < size; j++)
-	{
-		p = (j * size) + (size - l - j);
-		r += *(a + p);
-	}
-	printf("%i, %i\n", l, r);
+/**                                                                              * print_diagsums - Prints the sum of the two diagonals                          *             of a square matrix of integers.                                   * @a: The matrix of integers.                                                   * @size: The size of the matrix.                                                */
+ void print_diagsums(int *a, int size)                                           {
+	 int index, sum1 = 0, sum2 = 0;                                                  
+	 for (index = 0; index < size; index++)                                          {
+		 sum1 += a[index];                                                               a += size;
+	 }         
+         
+	 a -= size;                                                                      
+	 for (index = 0; index < size; index++)
+	 {
+         
+		 sum2 += a[index];                                                               a -= size;
+	 }
+         
+	 printf("%d, %d\n", sum1, sum2);                                                                                            
 }
+
