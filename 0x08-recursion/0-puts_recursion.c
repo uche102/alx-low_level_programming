ae6e1672a1ee91c5abc5fb5ab5fb496f3bf4c8e5
uche102 <nwakoruche192@gmail.com>
2023-01-06 03:17:02 +0100

    recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,6 +1,6 @@
-/*                                                                               * File: 0-puts_recursion.c                                                      * Auth: sam                                                                     */                                                                             
+/*                                                                               * File: 0-puts_recursion.c                                                      * Auth: sam                                                                     */
 
-#include "main.h"                                                               
+#include "main.h"
 
 /**                                                                              * _puts_recursion - Prints a string, followed by a new line.                    * @s: The string to be printed.                                                 */
 void _puts_recursion(char *s)
